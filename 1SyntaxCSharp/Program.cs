7a9269f9eb2f485dcbd80bed739ebe1f598a1ee7
can.d15 <can.d15@P-0G-0A002-0329>
2022-09-09 13:57:35 +0200

    Projektdateien hinzufügen.

---
+++ 1SyntaxCSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("1.Aufgabe (Geist)");
+
+
+Console.WriteLine(" .-.");
+Console.WriteLine("(o o)");
+Console.WriteLine("| O |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");
+
+
+
+Console.WriteLine("2.Aufgabe (Ausgabe Eingabe)");
+
+
+string Vorname = "Can";
+string Nachname = "Duman";
+string Alter = "16";
+string Stadt = "Köln";
+string plz = "50827";
+
+Console.WriteLine("Vorname: {0}", Vorname);
+Console.WriteLine("Nachneme: {0}", Nachname);
+Console.WriteLine("ALter: {0}", Alter);
+Console.WriteLine("Stadt: {0}", Stadt);
+Console.WriteLine("plz: {0}", plz);
+
+
+
+Console.WriteLine("3.Aufgabe (Ausgabe Eingabe)");
+
+
+Console.WriteLine("Geben sie ihren Vornamen ein");
+string vorname = Console.ReadLine();
+Console.WriteLine("Geben sie ihren Nachnamen ein");
+string nachname = Console.ReadLine();
+Console.WriteLine("Geben sie ihre Alter ein");
+string alter = Console.ReadLine();
+Console.WriteLine("Geben sie ihre Klasse ein");
+string klasse = Console.ReadLine();
+
+Console.WriteLine($"{vorname} ist {alter} Jahre alt und besucht die Klasse {klasse} des Georg-Simon-Ohm Berufskolleg.");
---
+++ Datentypen und Konvertierung/Program.cs
@@ -0,0 +1,82 @@
+﻿Console.WriteLine("1.Aufgabe");
+
+
+Console.WriteLine(short.MaxValue);
+Console.WriteLine(short.MinValue);
+/*
+    16 Bit
+    32767
+    -32768
+*/
+
+
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(int.MinValue);
+/*
+ * 2147483647
+-2147483648
+*/
+
+
+Console.WriteLine(long.MaxValue);
+Console.WriteLine(long.MinValue);
+/*
+ * 9223372036854775807
+-9223372036854775808
+*/
+
+
+Console.WriteLine(float.MaxValue);
+Console.WriteLine(float.MinValue);
+/*
+ * 3,4028235E+38
+-3,4028235E+38
+*/
+
+
+Console.WriteLine(double.MaxValue);
+Console.WriteLine(double.MinValue);
+/*
+ * 1,7976931348623157E+308
+-1,7976931348623157E+308
+*/
+
+
+Console.WriteLine(decimal.MaxValue);
+Console.WriteLine(decimal.MinValue);
+/*
+ * 79228162514264337593543950335
+-79228162514264337593543950335
+*/
+
+
+
+Console.WriteLine("2.Aufgabe (effizienteDeklarierung)");
+
+
+string vorname = "Can";
+short alter = 18;
+bool frage = true;
+
+Console.WriteLine("Ihre Antworten:");
+Console.WriteLine($"1. {vorname}\n2. {alter}\n3. {frage}");
+
+
+
+Console.WriteLine("3.Aufgabe (explizite Umwandlung)");
+
+
+int DollarZahl = 36;
+char Dollar = DollarZahl;
+
+
+
+Console.WriteLine("4.Aufgabe (‘Convert‘Klasse)");
+
+
+Console.WriteLine("Geben sie die erste Variable ein");
+string v1_str = Console.ReadLine();
+
+short v1_short = Convert.ToInt16(v1_str);
+
+Console.WriteLine(v1_short);
